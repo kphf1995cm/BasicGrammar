a3c62bd518d9207457d7d2611fff3db4d7763a8e
zero <kp18@mails.tsinghua.edu.cn>
2020-03-16 12:01:20 +0800

    add cplusplus11.h

--- cplusplus11.h
+++ cplusplus11.h
@@ -1,4 +1,29 @@
 # ifndef _CPLUSPLUS11_H
 # define _CPLUSPLUS11_H
+# include<iostream>
+# include<vector>
+# include<thread>
+class C11Feature {
+public:
+	// foreach sentence
+	void traverse() {
+		int nArr[5] = {1,2,3,4,5};
+		for (int &x : nArr) {
+			x *= 2;
+		}
+		for (int x : nArr) {
+			std::cout << x << " ";
+		}
+		std::cout << std::endl;
+	}
 
+	// auto
+	void testAuto() {
+		std::vector<int> vecs(10, 2);
+		for (auto i = vecs.begin(); i != vecs.end(); i++) {
+			std::cout << *i << " ";
+		}
+		std::cout << std::endl;
+	}
+};
 # endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 # include <iostream>
 # include "constructor.h"
 # include "pointer.h"
-using namespace std;
+# include "cplusplus11.h"
+# include "multithread.h"
 
 void testConstructor() {
 	A a;
@@ -12,7 +13,28 @@ void testPointer() {
 	wp.generate3();
 }
 
+void testC11Feature() {
+	C11Feature c11;
+	c11.traverse();
+	c11.testAuto();
+}
+
+void testThread() {
+	std::thread t1(func, 1);
+	std::thread t2([]() {
+		std::cout << "hello " << std::this_thread::get_id() << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		std::cout << "world " << std::this_thread::get_id() << std::endl;
+	});
+	ThreadTask task(1, 2);
+	std::thread t3(task);
+
+	t1.join();
+	t2.join();
+	t3.join();
+}
+
 int main() {
-	testPointer();
+	testThread();
 	return 0;
 }
